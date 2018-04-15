e3e7993b3dd7ce81869e404573a9e1a435becbea
CongCong0424 <37912819+CongCong0424@users.noreply.github.com>
2018-04-15 14:24:36 -0400

    Add files via upload

---
+++ Project3/src/bike/WarehouseManager.java
@@ -0,0 +1,66 @@
+package project.pkg3;
+
+/**
+ *
+ * @author Zhu Cong Qian
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * WarehouseManager class contains actions for a warehouse manager employee
+ *
+ * @author,
+ */
+public class WarehouseManager extends LoginAccount implements Serializable {
+
+    private String warehouseName;
+    private Warehouse warehouse;
+    private AccountType accountType;
+    ArrayList<Inventory> retList;
+
+    /**
+     * Warehouse Manager constructor
+     *
+     * @param p Person that is a warehouse manager
+     * @param aUserName Username of a warehouse manager
+     * @param aPassword Password of a warehouse manager
+     */
+    public WarehouseManager(Person p, String aUserName, String aPassword) {
+        super(p, aUserName, aPassword);
+        accountType = AccountType.WAREHOUSE_MANAGER;
+    }
+
+    /**
+     * Get warehouse name
+     *
+     * @return Warehouse name
+     */
+    public String getWarehouseName() {
+        return warehouseName;
+    }
+
+    /**
+     * Get warehouse
+     *
+     * @return Warehouse
+     */
+    public Warehouse getWarehouse() {
+        return warehouse;
+    }
+
+    /**
+     * Get account type
+     *
+     * @return Account type
+     */
+    @Override
+    public AccountType getType() {
+        return accountType;
+    }
+
+}
