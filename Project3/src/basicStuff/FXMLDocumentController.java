5a292c9ee29290b700cd7b649b969f663d3730b8
Jleetrahan <jtrahan@mail.umw.edu>
2018-04-23 18:57:59 -0400

    Create FXMLDocumentController.java

---
+++ Project3/src/basicStuff/FXMLDocumentController.java
@@ -0,0 +1,65 @@
+package basicStuff;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import static project2.BicycleParts.addPart;
+
+/**
+ *
+ * @author Jackson Trahan
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private ArrayList<BikePart> warehouse = new ArrayList();
+    @FXML
+    private TextField input;
+    @FXML
+    private TextArea output;
+    
+    @FXML
+    private void read(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void enter(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void sell(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void display(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    public void sortName(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    public void sortNum(ActionEvent event) {
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+}
