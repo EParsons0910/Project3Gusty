b78e652e9253a4569b237b5210cb65f54df8605e
Jleetrahan <jtrahan@mail.umw.edu>
2018-04-23 23:35:09 -0400

    Update OfficeMan.java

--- Project3/src/bike/OfficeMan.java
+++ Project3/src/bike/OfficeMan.java
@@ -7,17 +7,17 @@ import basicStuff.LoginAccount;
  * @author Elizabeth Parsons, Jackson Trahan
  */
 public class OfficeMan extends LoginAccount {
-    private String fn; 
-    private String ln;
-    private String em;
-    private String un;
-    private String pw;
+    String firstName; 
+    String lastName;
+    String email;
+    String username;
+    String password;
     
     public OfficeMan(String fn, String ln, String em, String un, String pw){
-        this.fn = fn;
-        this.ln = ln;
-        this.em = em;
-        this.un = un;
-        this.pw = pw;
+        firstName = fn;
+        lastName = ln;
+        email = em;
+        username = un;
+        password = pw;
     }
 }
