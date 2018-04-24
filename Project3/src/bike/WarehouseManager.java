757995034600ece9612b55cae8288b39522cc1ab
Jleetrahan <jtrahan@mail.umw.edu>
2018-04-23 23:32:01 -0400

    Major changes

--- Project3/src/bike/WarehouseManager.java
+++ Project3/src/bike/WarehouseManager.java
@@ -1,66 +1,23 @@
-package project.pkg3;
+package bike;
+
+import basicStuff.LoginAccount;
 
 /**
  *
- * @author Zhu Cong Qian
+ * @author Jackson Trahan
  */
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
-/**
- * WarehouseManager class contains actions for a warehouse manager employee
- *
- * 
- */
-public class WarehouseManager extends LoginAccount implements Serializable {
-
-    private String warehouseName;
-    private Warehouse warehouse;
-    private AccountType accountType;
-    ArrayList<Inventory> retList;
-
-    /**
-     * Warehouse Manager constructor
-     *
-     * @param p Person that is a warehouse manager
-     * @param aUserName Username of a warehouse manager
-     * @param aPassword Password of a warehouse manager
-     */
-    public WarehouseManager(Person p, String aUserName, String aPassword) {
-        super(p, aUserName, aPassword);
-        accountType = AccountType.WAREHOUSE_MANAGER;
+public class WarehouseManager extends LoginAccount {
+    String firstName;
+    String lastName;
+    String email;
+    String username;
+    String password;
+    
+    public WarehouseManager(String fn, String ln, String em, String un, String pw) {
+        firstName = fn;
+        lastName = ln;
+        email = em;
+        username = un;
+        password = pw;
     }
-
-    /**
-     * Get warehouse name
-     *
-     * @return Warehouse name
-     */
-    public String getWarehouseName() {
-        return warehouseName;
-    }
-
-    /**
-     * Get warehouse
-     *
-     * @return Warehouse
-     */
-    public Warehouse getWarehouse() {
-        return warehouse;
-    }
-
-    /**
-     * Get account type
-     *
-     * @return Account type
-     */
-    @Override
-    public AccountType getType() {
-        return accountType;
-    }
-
 }
