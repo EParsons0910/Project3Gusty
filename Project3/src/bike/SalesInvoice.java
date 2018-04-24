c25340c3d180fb02885a2b9a4f4f3f1e1c9030a2
EParsons0910 <37839307+EParsons0910@users.noreply.github.com>
2018-04-23 20:02:21 -0400

    Add files via upload

--- Project3/src/bike/SalesInvoice.java
+++ Project3/src/bike/SalesInvoice.java
@@ -10,16 +10,52 @@ package bike;
  * @author Elizabeth Parsons
  */
 import java.util.Date;
+import basicStuff.bikePart;
 
 public class SalesInvoice {
     private Date dateOfSale;
+    private bikePart bp;
+    private String empReceived;
+    private String salesAssociate;
+    private double totalCost;
+    private final String customer;
     
-    SalesInvoice(Date dateOfSale){
+    SalesInvoice(Date dateOfSale, String customer, bikePart bp, String empReceived, String salesAssociate, double totalCost){
+        this.bp = bp;
         this.dateOfSale = dateOfSale;
+        this.customer = customer;
+        this.empReceived = empReceived;
+        this.salesAssociate = salesAssociate;
+        this.totalCost = totalCost;
+        
     }
     
     public Date getDateOfSale(){
         return dateOfSale;
     }
     
+    public String getCustomer(){
+        return customer;
+    }
+    
+    public bikePart getBikePart(){
+        return bp;
+    }
+    
+    public String getEmpReceived(){
+        return empReceived;
+    }
+    
+    public String getSalesAssociate(){
+        return salesAssociate;
+    }
+    
+    public double getTotalCost(){
+        return totalCost;
+    }
+    
+    public String toString(){
+        return "Sales Invoice: " + "Sales Associate=" + salesAssociate + ", Date of Sale=" + dateOfSale + ", Bike Part Employee Who Received Parts="
+                + empReceived + ", Parts Sold=" + bp + ", Total Cost =" + totalCost + ".";
+    }
 }
