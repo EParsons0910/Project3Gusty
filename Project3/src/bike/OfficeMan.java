54a182661f84e6f51620670f58921918002f3a2f
Jleetrahan <jtrahan@mail.umw.edu>
2018-04-23 23:12:11 -0400

    Update OfficeMan.java

--- Project3/src/bike/OfficeMan.java
+++ Project3/src/bike/OfficeMan.java
@@ -1,22 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bike;
+
+import basicStuff.LoginAccount;
 
 /**
  *
- * @author Elizabeth Parsons
+ * @author Elizabeth Parsons, Jackson Trahan
  */
-public class OfficeMan {
+public class OfficeMan extends LoginAccount {
     private String fn; 
     private String ln;
     private String em;
     private String un;
     private String pw;
-    
-    
     
     public OfficeMan(String fn, String ln, String em, String un, String pw){
         this.fn = fn;
@@ -25,6 +20,4 @@ public class OfficeMan {
         this.un = un;
         this.pw = pw;
     }
-    
-    
 }
