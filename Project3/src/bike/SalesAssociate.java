c353ac1dd0ddf19e04a7bae56ac4d6236bc8c5cd
Jleetrahan <jtrahan@mail.umw.edu>
2018-04-23 23:40:09 -0400

    Update SalesAssociate.java

--- Project3/src/bike/SalesAssociate.java
+++ Project3/src/bike/SalesAssociate.java
@@ -13,7 +13,7 @@ public class SalesAssociate extends LoginAccount {
     String username;
     String password;
     
-    SalesAssociate(String fn, String ln, String em, String un, String pw){
+    public SalesAssociate(String fn, String ln, String em, String un, String pw){
         firstName = fn;
         lastName = ln;
         email = em;
