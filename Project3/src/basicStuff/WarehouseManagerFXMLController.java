9fe6f28bd67bcb1d0ff63b3d27d0b46644d69fd0
EParsons0910 <37839307+EParsons0910@users.noreply.github.com>
2018-04-24 01:16:15 -0400

    Warehouse Manager GUI

---
+++ Project3/src/basicStuff/WarehouseManagerFXMLController.java
@@ -0,0 +1,37 @@
+package basicStuff;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Elizabeth Parsons
+ */
+public class WarehouseManagerFXMLController implements Initializable {
+    @FXML
+    private void nameSearch(ActionEvent event) {
+        
+    }
+    
+    @FXML
+    private void numSearch(ActionEvent event) {
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+}
