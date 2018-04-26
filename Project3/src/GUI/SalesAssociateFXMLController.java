5ac5f93d5620ddf8d7e36fdce5e93a102c860495
EParsons0910 <37839307+EParsons0910@users.noreply.github.com>
2018-04-26 01:36:52 -0400

    Basic Sales Associate GUI

---
+++ Project3/src/GUI/SalesAssociateFXMLController.java
@@ -0,0 +1,28 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SalesAssociateFXMLController {
+
+    @FXML
+    private AnchorPane LoadSalesVanPane;
+
+    @FXML
+    private TextField transferFile;
+
+    @FXML
+    private Button okTransfer;
+
+    @FXML
+    private AnchorPane CreateSalesInvoicePane;
+
+    @FXML
+    private DatePicker pickADate;
+
+    @FXML
+    private Tab logOutTab;
+
+}
